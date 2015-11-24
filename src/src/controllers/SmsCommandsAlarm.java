1e125ead36d4f0175a4698ad750aa98fb873dd95
Yungamm <yungamm@yandex.ru>
2015-11-24 13:46:15 +0300

    add set & query metods

--- src/src/controllers/SmsCommandsAlarm.java
+++ src/src/controllers/SmsCommandsAlarm.java
@@ -4,12 +4,12 @@ import org.jetbrains.annotations.NotNull;
 import src.model.BasicSettings;
 import src.model.Relay;
 
-public class CommandsAlarm {
+public class SmsCommandsAlarm {
     private String command;
 
     //метод возвращает команду Реле для СМС
     @NotNull
-    public static String outRelaySms(Relay relay) {
+    public static String setRelay(Relay relay) {
         StringBuilder sb = new StringBuilder();
 
         sb.append("#R");
@@ -24,9 +24,24 @@ public class CommandsAlarm {
         return sb.toString();
     }
 
-    @NotNull
-    public static String outSetTimeDate(BasicSettings basicSettings){
+    public static String setTimeDate(BasicSettings basicSettings){
         return "V#D=" + basicSettings.getTimeInDevice() + "," + basicSettings.getDateInDevice();
     }
 
+    public static String setPhoneSetting(final int countPhone){
+        return "";
+    }
+    public static String queryRele(final String command){
+        return command.equals("R2") ? "?R2" : ""; //вставить команду запроса первых реле
+    }
+
+    public static String queryFull(){
+        return  "V?Z";
+    }
+
+    public static String queryPhone(){
+        return "?4";
+    }
+
+    public static String queryDeviceStatus(){ return "?5";}
 }
